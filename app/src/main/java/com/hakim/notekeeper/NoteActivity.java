3d6d19b339cd19df2f37ebc309872f0cd09b73bc
hakimks <hakimks@gmail.com>
2019-04-24 12:53:25 +0300

    refractor: rename xml files to match app name

--- app/src/main/java/com/hakim/notekeeper/NoteActivity.java
+++ app/src/main/java/com/hakim/notekeeper/NoteActivity.java
@@ -14,7 +14,7 @@ public class NoteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.note_main);
+        setContentView(R.layout.activity_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
