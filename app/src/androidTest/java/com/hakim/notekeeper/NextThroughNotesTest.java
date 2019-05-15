9c6ca2de53918f10bcee6930c17db320a7c7f831
hakimks <hakimks@gmail.com>
2019-05-15 20:24:20 +0300

    test: add step tp go through from opening the drawer, selecting notes on menu , displaying the list of notes and selecting the firt note in the list

--- app/src/androidTest/java/com/hakim/notekeeper/NextThroughNotesTest.java
+++ app/src/androidTest/java/com/hakim/notekeeper/NextThroughNotesTest.java
@@ -1,10 +1,14 @@
 package com.hakim.notekeeper;
 
 import android.support.test.espresso.contrib.DrawerActions;
+import android.support.test.espresso.contrib.NavigationViewActions;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -25,5 +29,17 @@ public class NextThroughNotesTest {
     @Test
     public void nextThroughNotes(){
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+
+        NoteInfo note = notes.get(index);
+
+        onView(withId(R.id.spinner_course)).check(matches(withSpinnerText(note.getCourse().getTitle())));
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_content)).check(matches(withText(note.getText())));
     }
 }
