8dacd7f856d76c3a10972ec862191d96a88f7efc
hakimks <hakimks@gmail.com>
2019-05-20 14:21:12 +0300

    fix: The settings Screen is displaying the correct summary and also reflects the changes made

--- app/src/main/java/com/hakim/notekeeper/SettingsActivity.java
+++ app/src/main/java/com/hakim/notekeeper/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.hakim.notekeeper;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import android.content.SharedPreferences;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +29,51 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat  implements SharedPreferences.OnSharedPreferenceChangeListener {
+        SharedPreferences sSharedPreferences;
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+
+            onSharedPreferenceChanged(sSharedPreferences, "user_display_name");
+            onSharedPreferenceChanged(sSharedPreferences, "user_email_address");
+            onSharedPreferenceChanged(sSharedPreferences, "user_favorite_social");
+
 
 
         }
 
         @Override
-        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+            Preference preference = findPreference(key);
+            if (preference instanceof ListPreference) {
+                ListPreference listPreference = (ListPreference) preference;
+                int prefIndex = listPreference.findIndexOfValue(sharedPreferences.getString(key, ""));
+                if (prefIndex >= 0) {
+                    preference.setSummary(listPreference.getEntries()[prefIndex]);
+                }
+            } else {
+                preference.setSummary(sharedPreferences.getString(key, ""));
+
+            }
+
 
         }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            // Set up a listener whenever a key changes
+            getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        }
+
     }
 }
