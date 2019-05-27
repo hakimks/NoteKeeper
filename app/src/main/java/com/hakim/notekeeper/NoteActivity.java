2384b9bb864a0eeb90afdffb97ebb38d196e0ad4
hakimks <hakimks@gmail.com>
2019-05-27 15:03:30 +0300

    feat: added ability to save a note to the database instead of to memory

--- app/src/main/java/com/hakim/notekeeper/NoteActivity.java
+++ app/src/main/java/com/hakim/notekeeper/NoteActivity.java
@@ -1,6 +1,7 @@
 package com.hakim.notekeeper;
 
 import android.app.LoaderManager;
+import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
@@ -196,10 +197,34 @@ public class NoteActivity extends AppCompatActivity implements LoaderManager.Loa
 
     private void saveNote() {
 
-//        mNote.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());
-//
-//        mNote.setTitle(mTextNoteTitle.getText().toString());
-//        mNote.setText(mTextNoteText.getText().toString());
+       String courseId = selectedCourseId();
+
+        String noteTitle =  mTextNoteTitle.getText().toString();
+        String noteText =  mTextNoteText.getText().toString();
+
+        saveNoteToDatabase(courseId, noteTitle, noteText);
+    }
+
+    private String selectedCourseId() {
+        int selectedPosition = mSpinnerCourses.getSelectedItemPosition();
+        Cursor cursor = mAdapterCourse.getCursor();
+        cursor.moveToPosition(selectedPosition);
+        int courseIdPos = cursor.getColumnIndex(NoteInfoEntry.COLUMN_COURSE_ID);
+        String courseId = cursor.getString(courseIdPos);
+        return courseId;
+    }
+
+    private void saveNoteToDatabase(String courseId, String noteTitle, String noteText){
+        String selection = NoteInfoEntry._ID + " =? ";
+        String[] selectionArgs = {Integer.toString(mNoteId)};
+
+        ContentValues values = new ContentValues();
+        values.put(NoteInfoEntry.COLUMN_COURSE_ID, courseId);
+        values.put(NoteInfoEntry.COLUMN_NOTE_TITLE, noteTitle);
+        values.put(NoteInfoEntry.COLUMN_NOTE_TEXT, noteText);
+
+        SQLiteDatabase db = mOpenHelper.getWritableDatabase();
+        db.update(NoteInfoEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 
     private void displayNote() {
